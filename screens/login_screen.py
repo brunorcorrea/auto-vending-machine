34889a9019eeffe65ea91812cedb9f3491f1eb1c
brunorcorrea <correaricardobruno@gmail.com>
2022-11-10 16:18:38 -0300

    adding textInputs

--- screens/login_screen.py
+++ screens/login_screen.py
@@ -19,19 +19,57 @@ def build_login_screen(main_layout):
     )
 
     email_label = Label(
-            text='Email',
+            text='Email:',
             font_size=24,
             color=(0, 0, 0, 1),
-            pos=(1440 / 2 + 1440 * 0.2, 1024 / 8)
+            pos=(0, 100)
         )
 
     layout.add_widget(
         TextInput(
-            pos=(0, 1024 / 8)
+            foreground_color=(1, 1, 1, 1),
+            multiline=False,
+            size_hint=(.2, None),
+            height=30,
+            background_normal='',
+            background_color=(0, 0, 0, 1),
+            pos=(0, 100)
         )
+    )
+
+    password_label = Label(
+        text='Senha:',
+        font_size=24,
+        color=(0, 0, 0, 1),
+        pos=(0, 0)
+    )
+
+    layout.add_widget(
+        TextInput(
+            foreground_color=(1, 1, 1, 1),
+            multiline=False,
+            size_hint=(.2, None),
+            height=30,
+            background_normal='',
+            background_color=(0, 0, 0, 1),
+            pos=(0, 200)
+        )
+    )
+
+    login_button = Button(
+        text='Login',
+        font_size=32,
+        bold=True,
+
+        size_hint=(.15, .10),
+        pos=(0, 0),
+        background_normal='',
+        background_color=(0.66, 0.33, 0.33, 1)
     )
 
     layout.add_widget(store_name_label)
     layout.add_widget(email_label)
+    layout.add_widget(password_label)
+    layout.add_widget(login_button)
 
     return layout
